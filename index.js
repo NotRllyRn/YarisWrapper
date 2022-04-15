5b9e37f85bda07f80bdf97151687255ac22b305d
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-04-15 00:02:11 -0700

    weeeeeeee, fixed a few things and added a new sustem of working

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ class Yaris {
     constructor(API_KEY) {
         this.#API_KEY = API_KEY;
 
-        this.request('POST', 'info', {}).then(res => res.json()).then(data => {
-            if (data && data.information) {
+        this.#request('POST', 'info', {}).then(data => {
+            if (!data.error) {
                 this.#INFORMATION = data.information;
-            } else throw new Error('[Yaris-Wrapper] new constructor error: ' + data.error.message);
-        }).catch(err => console.error(err));
+            } else throw new Error('[Yaris-Wrapper] new constructor error: ' + data.error);
+        }).catch(err => { throw err });
 
         return this
     }
     #API_KEY = "";
     #INFORMATION;
-    request = (method = 'GET', endpoint, data = {}) => {
+    #request = (method = 'GET', endpoint, data = {}) => {
         const req = {
             method: method,
             headers: {
@@ -23,7 +23,12 @@ class Yaris {
         }
         if (method == 'POST') req.body = JSON.stringify(data);
 
-        return fetch('https://api.yaris.rocks/v1/' + endpoint, req)
+        return new Promise(resolve => {
+            fetch('https://api.yaris.rocks/v1/' + endpoint, req).then(res => res.json()).then(data => {
+                if (data.success || data.information) resolve(data.information);
+                else resolve({ success: false, error: data.error.message });
+            }) 
+        })
     }
     getInfo(callback) {
         const info =  new Promise((resolve, _) => {
@@ -31,71 +36,51 @@ class Yaris {
                 if (this.#INFORMATION) {
                     resolve(this.#INFORMATION)
                 }
-            }, 100)
+            }, 1)
         })
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     addUser = async (data, callback) => {
-        const info = this.request('POST', 'adduser', data).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', 'adduser', data)
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     removeUser = async (data, callback) => {
-        const info = this.request('POST', 'removeuser', data).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', 'removeuser', data)
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     getUser = async (tag, callback) => {
-        const info = this.request('POST', `getuser/${tag}`, {}).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', `getuser/${tag}`, {})
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     whitelistUser = async (hwid, callback) => {
-        const info = this.request('POST', 'whitelistuser', { data: hwid }).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', 'whitelistuser', { data: hwid })
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     blacklistUser = async (hwid, reason, callback) => {
-        const info = this.request('POST', 'blacklistuser', { data: hwid, reason: reason }).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', 'blacklistuser', { data: hwid, reason: reason })
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
-    addKey = async (data = {}, callback) => {
-        const info = this.request('POST', 'addkey', data).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+    addKey = async (callback) => {
+        const info = this.#request('POST', 'addkey', {})
         if (callback) {
+            console.log(callback)
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
     }
     removeKey = async (key, callback) => {
-        const info = this.request('POST', 'removekey', { key: key }).then(res => res.json()).then(data => {
-            if (data && data.information) return data.information;
-            else return { success: false, error: data.error.message };
-        })
+        const info = this.#request('POST', 'removekey', { key: key })
         if (callback) {
             info.then(data => callback(data)).catch(err => console.error(err))
         } else return info;
